6d0dc562b4cbf3fa7b60861f0d0f0db494a43dc0
longnh <hoanglong180695@gmail.com>
2023-02-19 17:54:03 +0700

    update

--- src/main/scala/job/sql/Demo.scala
+++ src/main/scala/job/sql/Demo.scala
@@ -3,7 +3,7 @@ package job.sql
 import java.util.Properties
 
 import job.JobInf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SaveMode, SparkSession}
 
 object Demo extends JobInf {
    def run(spark: SparkSession, properties: Properties): Unit = {
@@ -11,6 +11,6 @@ object Demo extends JobInf {
      val numAs = logData.filter(line => line.contains("a")).count()
      val numBs = logData.filter(line => line.contains("b")).count()
      val df = spark.createDataFrame(Seq((numAs, numBs))).toDF("Num a", "Num b")
-     df.write.json(properties.getProperty("output"))
+     df.write.mode(SaveMode.Overwrite).json(properties.getProperty("output"))
   }
 }
